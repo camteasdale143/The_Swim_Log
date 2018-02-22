5ead3aec62e529f510a47e00c2164face2bac527
Cameron Teasdale <camteasdale143@gmail.com>
2018-02-22 22:31:09 +0000

    add full mlab database

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose")
 
-mongoose.connect("mongodb://yelpcamp:colt@ds113835.mlab.com:13835/yelp_camp_cameron_teasdale");
+//mongoose.connect("mongodb://yelpcamp:colt@ds113835.mlab.com:13835/yelp_camp_cameron_teasdale");
+mongoose.connect("mongodb://camteasdale:waper143@ds147408-a0.mlab.com:47408,ds147408-a1.mlab.com:47408/yelp_camp_cameron_teasdale?replicaSet=rs-ds147408")
 //mongoose.connect("mongodb://localHost/swimLogs")
 
 mongoose.promise = Promise;
